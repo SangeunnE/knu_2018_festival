5ef9650c06ca85ec49519cc98fe2167d0c1e8507
SangeunnE <sangeunne@likelion.org>
2018-09-10 14:35:23 +0000

    STARTgit add .!

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180910125905) do
   end
 
   create_table "messages", force: :cascade do |t|
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
